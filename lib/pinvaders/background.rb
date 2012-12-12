4efbfc087c4d188ca97e5b6639f2bf90f14e40fa
Shazad Rehman <punjabdapunk@gmail.com>
2012-12-12 19:28:18 +0000

    remove corrupted star file

--- lib/pinvaders/background.rb
+++ lib/pinvaders/background.rb
@@ -2,14 +2,14 @@ module Pinvaders
   class Background
     def initialize
       @stars = []
-      1.times do |i| 
+      1.times do |i|
         @stars[i] = Star.new
-        @stars[i].pos(1 + rand(Curses::cols), 0) 
+        @stars[i].pos(1 + rand(Curses::cols), 0)
       end
     end
 
     def draw
-      @stars.each do |star| 
+      @stars.each do |star|
         star.draw 
         if not star.on_screen
           star.reset
